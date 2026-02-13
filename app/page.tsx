3e8b2423a2dfa7c734d72291df6ecca03b47c7bd
“[ritu <ritu748841@gmail.com>
2026-02-13 13:11:48 +0530

    upgrade

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-green-500 h-[100vh] text-4xl text-white'>
+      Agrovision AI
+    </div>
+  )
+}
+
+export default page
