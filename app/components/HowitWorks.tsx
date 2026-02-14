250e60a48321a2b53ebe6aff9ec329136a1a0fed
“[ritu <ritu748841@gmail.com>
2026-02-14 09:51:57 +0530

    feat: create new How It Works section

---
+++ app/components/HowitWorks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HowitWorks = () => {
+  return (
+    <div>
+      hhhhhhh
+    </div>
+  )
+}
+
+export default HowitWorks
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Chatbot from "./components/Chatbot";
+import HowitWorks from "./components/HowitWorks";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white dark:bg-[#0f1f14] transition-colors duration-300">
       <Navbar />
       <Hero />
       <Chatbot />
+      <HowitWorks />
 
 
 
